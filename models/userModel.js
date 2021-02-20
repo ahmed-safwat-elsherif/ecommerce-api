933c648704b9e6bc45a613843c5e20ddd9f66b21
Ahmed Elsherif <s-aelsherif500@zewailcity.edu.eg>
2021-02-20 21:40:03 +0200

    Feat:

--- models/userModel.js
+++ models/userModel.js
@@ -35,10 +35,6 @@ const userSchema = Schema({
         type:String,
         minlength:3
     }],
-    profileImage:{
-        type:String,
-        default:''
-    },
     phones:[{
         type:String,
         validate:/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g
