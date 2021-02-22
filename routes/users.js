831729cbed01211e5ba597b0b4a5c7dd7c37185a
Ahmed Elsherif <s-aelsherif500@zewailcity.edu.eg>
2021-02-22 05:26:45 +0200

    feat: orderModule post order

--- routes/users.js
+++ routes/users.js
@@ -75,7 +75,7 @@ router.get('/confirmation/:token', async (req, res) => {
         const user = await User.findOneAndUpdate({ _id }, { confirmation: true }, {
             new: true
         }).exec();
-        res.redirect('https://amnesia-skincare.herokuapp.com/confirmed')
+        res.redirect('https://amnesia-skincare.herokuapp.com/views/confirmed')
         res.status(200).send({ user, success: true, message: "User is confirmed!" })
     } catch (error) {
         res.status(400).send({ error, success: false, message: "Confirmation is denied!" })
@@ -140,6 +140,73 @@ router.patch('/changePassword', authenticate, async (req, res) => {
         res.status(200).send({ newUpdate, message: "password has been changed successfully", success: true })
     } catch (error) {
         res.status(400).send({ error, message: "Failure in changing password", success: false })
+    }
+})
+
+router.post('/forgetPassword',async(req,res)=>{
+    try {
+        let {email} = req.body;
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(404).send({message:"Email is not registered", success:false})
+        }
+        const token = jwt.sign({ _id: user._id }, 'the-attack-titan',{ expiresIn: 60 * 60 * 2 }); // expiration json web token in 2 hours
+        const forgetPassword = `https://amnesia-skincare.herokuapp.com/views/resetpassword/${token}`;
+        const message = `
+        <div style="padding:30px 0 ;font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;text-align: center; background-color:#eae3c8; color:#383e56; border-radius: 5px;">
+            <p style="font-size:1.3rem; font-weight:bold">
+                Welcome to <span style="font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: coral;">Amnesia</span> 
+            </p>
+            <hr style="width:50%;"/>
+            <h3 style="text-align: left; padding-left: 50px; margin-bottom: 2rem;">Hi, ${user.firstname}</h3>
+            <div style="font-size: 1rem; padding-left: 50px; padding-bottom:20px; text-align: left;">
+                <p>
+                    A password reset event has been triggered. The password reset window is limited to two hours. If you do not reset your password within two hours, you will need to submit a new request. To complete the password reset process, visit the following link:
+                </p>
+            </div>
+            <p style="text-align: center;">
+                <a style="display: inline-block; padding:10px;background-color:green; color:white;text-decoration: none;cursor: pointer;box-shadow: 0 0 8px gray;" href="${forgetPassword}">Reset password</a>
+            </p>
+        </div>
+        `
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'amnesia.ecommerce@gmail.com',
+                pass: process.env.GMAIL_PASS || '0159357eE'  // need to be saved somewhere else to achieve the security
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Amnesia - Skin Care" <amnesia.ecommerce@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: "Confirmation", // Subject line
+            text: "Confirmation message", // plain text body
+            html: message, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        res.status(201).send({ user, success: true, message: "sent successfully" })
+    } catch (error) {
+        res.status(404).send({message:"Unable to reset password", success:false,error})
+    }
+})
+
+router.get('/reset/password/',authenticate,async(req,res)=>{
+    try {
+        let {_id} = req.signData;
+        let {password} = req.body;
+        let user = await User.findByIdAndUpdate({_id},{password},{
+            new:true
+        }).exec();
+        res.status(200).send({message:"Password has been changed successfully",success:true,user})
+    } catch (error) {
+        res.status(400).send({message:"Password failed to be changed",success:false,error})
     }
 })
 router.route('/')
