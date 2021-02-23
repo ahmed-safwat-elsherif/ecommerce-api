0a419b886b7535ea7a7771ce04bedbad912a5bda
Ahmed Elsherif <s-aelsherif500@zewailcity.edu.eg>
2021-02-23 02:46:14 +0200

    feat: contact us

--- routes/users.js
+++ routes/users.js
@@ -158,7 +158,7 @@ router.post('/forgetPassword',async(req,res)=>{
             return res.status(404).send({message:"Email is not registered", success:false})
         }
         const token = jwt.sign({ _id: user._id }, 'the-attack-titan',{ expiresIn: 60 * 60 * 2 }); // expiration json web token in 2 hours
-        const forgetPassword = `https://amnesia-skincare.herokuapp.com/views/resetpassword/${token}`;
+        const forgetPassword = `http:localhost:4200/resetpassword/${token}`;
         const message = `
         <div style="padding:30px 0 ;font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;text-align: center; background-color:#eae3c8; color:#383e56; border-radius: 5px;">
             <p style="font-size:1.3rem; font-weight:bold">
