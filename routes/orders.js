aba858e4e3d02b409527074cc95e4784848231a0
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-02-24 01:23:38 +0200

    feat: dummy

--- routes/orders.js
+++ routes/orders.js
@@ -25,7 +25,7 @@ router.get('/',authenticate,async(req,res)=>{
         let userId = req.signData._id;
         console.log(userId,req.body)
         let {_id} = req.body;
-        let order = await Order.findOne({_id}).populate('productId');
+        let order = await Order.findOne({_id}).populate('products.productId');
         res.status(200).send({order,success:true})
     } catch (error) {
         res.status(401).send({error,message:"Unable to get order",success:false})
