902e09dcbabaa0218fd85383b840329636212ec7
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-04 21:47:42 +0200

    feat: body-parser

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const methodOverride = require('method-override');
 const app = express();
-app.use(cors()) // enable it for all routes
 const port = 3000;
 require('./db-connection');
 app.use(bodyParser.json({
     limit: '50mb'
-  }));
-  
-  app.use(bodyParser.urlencoded({
+}));
+
+app.use(bodyParser.urlencoded({
     limit: '50mb',
     parameterLimit: 100000,
-    extended: true 
-  }));
+    extended: true
+}));
+app.use(cors()) // enable it for all routes
 app.use(express.json());
 
 app.use(methodOverride('_method'))
@@ -27,17 +27,17 @@ const products = require('./routes/products');
 const orders = require('./routes/orders');
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     const requestDate = Date.now();
-    console.log({method: req.method, URL: req.url, Time: requestDate});
+    console.log({ method: req.method, URL: req.url, Time: requestDate });
     next()
 })
 // ------------ Routes -------------------------------------------------------------------------------
 
 app.use('/api/users', users);
-app.use('/api/images',uploads);
-app.use('/api/products',products);
-app.use('/api/orders',orders);
+app.use('/api/images', uploads);
+app.use('/api/products', products);
+app.use('/api/orders', orders);
 
 //----------------------------------------------------------------------------------------------------
 
@@ -46,6 +46,6 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Something broke!')
 })
 
-app.listen(process.env.PORT||port,()=>{
+app.listen(process.env.PORT || port, () => {
     console.log(`Server is listening on http://localhost:${port}/`);
 })
