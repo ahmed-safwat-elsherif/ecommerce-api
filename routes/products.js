e4cd2f5eaef89f500d7169e3666b173d7b7dbf77
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-06 22:13:11 +0200

    feat

--- routes/products.js
+++ routes/products.js
@@ -11,9 +11,10 @@ router.get('/',async(req,res)=>{
         if (Number(limit) > 10) {
             limit = 10;
         }
+        let numOfProducts =  await Product.countDocuments().exec();
         let products =  await Product.find().skip(Number(skip)).limit(Number(limit)).exec();
         if(!products) throw new Error(`Unabled to find any country to display`)
-        res.status(200).send({ length: products.length, products })
+        res.status(200).send({ length: numOfProducts, products })
     } catch (error) {
         res.status(401).send(error)
     }
