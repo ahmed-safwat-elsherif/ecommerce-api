e4ab59fe7d898797b3a2b9fc9a705527ee247cb1
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-07 18:35:33 +0200

    feat: ay habal

--- auth/user.js
+++ auth/user.js
@@ -3,31 +3,31 @@ const User = require('../models/userModel');
 const mongoose = require('mongoose')
 module.exports.authenticate = (req, res, next) => {
     try {
-        const {authorization} = req.headers;
+        const { authorization } = req.headers;
         console.log(authorization)
         const signData = jwt.verify(authorization, 'the-attack-titan');
-        console.log("signData:",signData)
+        console.log("signData:", signData)
         req.signData = signData;
         next();
     } catch (error) {
-        res.status(401).send({success: false, error:"Authorization failed"});
+        res.status(401).send({ success: false, error: "Authorization failed" });
     }
 }
 module.exports.adminAuthenticate = async (req, res, next) => {
     try {
-        const {authorization} = req.headers;
+        const { authorization } = req.headers;
         let _id = req.signData._id;
-        console.log("_id:",_id)
+        console.log("_id:", _id)
         _id = mongoose.Types.ObjectId(_id)
-        console.log("_id:",_id)
-        let user = await User.findById({_id});
-        if(user){
-            if(!user.isAdmin){
+        console.log("_id:", _id)
+        let user = await User.findById({ _id });
+        if (user) {
+            if (!user.isAdmin) {
                 console.log("Is not admin")
                 try {
-                    console.log(res)
-                    next(error)
-                    // res.status(401).send({success:false,message:"Admin Authentication failed"})
+                    // console.log(res)
+                    res.send({ success: false, message: "Admin Authentication failed" })
+                    // next(error)
                 } catch (error) {
                     console.log(error)
                 }
@@ -36,9 +36,9 @@ module.exports.adminAuthenticate = async (req, res, next) => {
                 next()
             }
         } else {
-            return res.status(401).send({success:false,message:"Admin Authentication failed"})
+            return res.status(401).send({ success: false, message: "Admin Authentication failed" })
         }
     } catch (error) {
-        res.status(404).send({success:false,error,message:"Authentication failed"})
+        res.status(404).send({ success: false, error, message: "Authentication failed" })
     }
 }
