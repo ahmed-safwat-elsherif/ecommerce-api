e31657855857bc57baf6d877d7424257beddcb54
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-17 18:11:51 +0200

    delete images with deleting products

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const Product = require('../models/productModel');
 const Image  = require('../models/imageModel')
 const ImageChunk  = require('../models/imageChunkModel')
 // get all products (from 0 to 15 with a skip)
-router.get('/:pname/',async(req,res)=>{
+router.get('/:pname/search',async(req,res)=>{
     try {
         let {pname} = req.params;
         console.log(pname)
@@ -31,6 +31,8 @@ router.get('/:pname/',async(req,res)=>{
         res.status(401).send(error)
     }
 })
+
+
 
 router.get('/get/all',async(req,res)=>{
     try {
