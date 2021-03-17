35aa7c51c6a4867dd0e8ee485528194edcd4c692
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-17 14:44:10 +0200

    delete images with deleting products

--- routes/products.js
+++ routes/products.js
@@ -46,7 +46,7 @@ router.get('/noOfRecords', async (req,res)=>{
 router.post('/product',authenticate,adminAuthenticate, async(req,res)=>{
     try {
         let createdBy = req.signData._id;
-        let {name,description, current_price=0,old_price=0} = req.body;
+        let {name,description, current_price=0,old_price=0,status="normal"} = req.body;
         console.log(current_price,old_price)
         current_price = Number(current_price);
         old_price = Number(old_price);
